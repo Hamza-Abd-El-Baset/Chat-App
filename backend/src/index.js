6aaa28f20ccee8c327d1ee81b29f58ccdfedb145
Hamza Abd-El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2025-02-11 10:12:14 +0200

    Static folder is is indicated for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,13 @@ import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js"
 import errorHandler from "./middlewares/errorHandler.js"
 import { app, server } from "./lib/socket.js"
+import path from "path"
 
 dotenv.config()
 
 const PORT = process.env.PORT
+const __dirname = path.resolve()
+
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
@@ -23,6 +26,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/profile", profileRoutes)
 app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.use(errorHandler)
 
